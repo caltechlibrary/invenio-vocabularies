98268a876f128e6d07e1b8d6afecc69d16a06b03
Tom Morrell <tmorrell@caltech.edu>
2024-02-09 15:49:13 -0800

    Revert "funding: allow custom funder name"
    This reverts commit f012e9b506856fc390f8a65ecbbeaf62dae3bf77.

--- invenio_vocabularies/assets/semantic-ui/js/invenio_vocabularies/src/contrib/forms/Funding/CustomAwardForm.js
+++ invenio_vocabularies/assets/semantic-ui/js/invenio_vocabularies/src/contrib/forms/Funding/CustomAwardForm.js
@@ -71,13 +71,12 @@ function CustomAwardForm({ deserializeFunder, selectedFunding }) {
         label={i18next.t("Funder")}
         noQueryMessage={i18next.t("Search for funder...")}
         clearable
-        allowAdditions
+        allowAdditions={true}
         multiple={false}
         selectOnBlur={false}
         selectOnNavigation={false}
         required
         search={(options) => options}
-        isFocused
         onValueChange={({ formikProps }, selectedFundersArray) => {
           if (selectedFundersArray.length === 1) {
             const selectedFunder = selectedFundersArray[0];
